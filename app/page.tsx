5dec6857d02dff1170ee3f87ac1e1ca33c0c47b7
Saeed-Ahmad-20 <81083938+Saeed-Ahmad-20@users.noreply.github.com>
2026-04-19 03:18:10 +0100

    adjust calendar icon

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
 
         <div className="flex items-center gap-2 mb-7 text-zinc-500">
           <svg
-            className="w-7 h-7 text-[#C4A348] shrink-0"
+            className="w-12 h-12 text-[#C4A348]"
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
