de59be1758780da9a7148e4e1fdb97e85701d87f
Saeed-Ahmad-20 <81083938+Saeed-Ahmad-20@users.noreply.github.com>
2026-04-20 17:24:06 +0100

    force light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,6 +47,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      style={{ colorScheme: 'light' }}
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-[#F8F7F3]">
